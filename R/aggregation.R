71e551803244d4b352fcea32c9fe4a318f32e173
bnoreen <bryannoreen@gmail.com>
2018-06-04 22:22:19 -0400

    Adding by_player and by_team aggregations
    Adding by_player and by_team aggregations. Simple codes but useful for
    my needs.

---
+++ R/aggregation.R
@@ -0,0 +1,49 @@
+#' by_player function
+#'
+#' This function takes a dataframe from advanced_stats or box_stats and aggregates.
+#' @param dframe the dataframe returned in advanced_stats or box_stats
+#' @param type Options are sum, mean, max, min
+#' @keywords ncaa, baseball, college
+#' @export
+#' @examples by_player(df,'sum')
+#' by_player()
+by_player <- function(dframe,type='sum'){
+  if(type=='sum'){
+    final <- dframe %>% group_by(Team,Player) %>% summarise_if(is.numeric,sum)
+  } else if (type=='mean'){
+    final <-dframe %>% group_by(Team,Player) %>% summarise_if(is.numeric,sum)
+  } else if (type == 'max'){
+    final <-dframe %>% group_by(Team,Player) %>% summarise_if(is.numeric,sum)
+  } else if (type == 'min'){
+    final <-dframe %>% group_by(Team,Player) %>% summarise_if(is.numeric,sum)
+  } else {
+    stop('type needs to be sum, mean, min, or max.')
+  }
+  return(as.data.frame(final))
+}
+
+
+#' by_team function
+#'
+#' This function takes a dataframe from advanced_stats or box_stats and aggregates.
+#' @param dframe the dataframe returned in advanced_stats or box_stats
+#' @param type Options are sum, mean, max, min
+#' @keywords ncaa, baseball, college
+#' @export
+#' @examples by_team(df,'sum')
+#' by_team()
+by_team <- function(dframe,type='sum'){
+  if(type=='sum'){
+    final <- dframe %>% group_by(Team) %>% summarise_if(is.numeric,sum)
+  } else if (type=='mean'){
+    final <-dframe %>% group_by(Team) %>% summarise_if(is.numeric,sum)
+  } else if (type == 'max'){
+    final <-dframe %>% group_by(Team) %>% summarise_if(is.numeric,sum)
+  } else if (type == 'min'){
+    final <-dframe %>% group_by(Team) %>% summarise_if(is.numeric,sum)
+  } else {
+    stop('type needs to be sum, mean, min, or max.')
+  }
+  return(as.data.frame(final))
+}
+
